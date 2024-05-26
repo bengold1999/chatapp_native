c329076a83715bfdceb4bd7c2c9ef4bbeb721632
bengold1999 <bengold1999@gmail.com>
2024-05-26 16:24:31 +0300

    Add chat and base Home

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from "react";
+import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { FontAwesome } from '@expo/vector-icons';
+// import colors from '../colors';
+import { Entypo } from '@expo/vector-icons';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
 
-export default Home
+    const navigation = useNavigation();
 
-const styles = StyleSheet.create({})
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <FontAwesome name="search" size={24}  style={{marginLeft: 15}}/>
+            ),
+        });
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => navigation.navigate("Chat")}
+                style={styles.chatButton}
+            >
+                <Entypo name="chat" size={24}  />
+            </TouchableOpacity>
+        </View>
+    );
+    };
+
+    export default Home;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            backgroundColor: "#fff",
+        },
+        chatButton: {
+            // backgroundColor: colors.primary,
+            height: 50,
+            width: 50,
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            // shadowColor: colors.primary,
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: .9,
+            shadowRadius: 8,
+            marginRight: 20,
+            marginBottom: 50,
+        }
+    });
